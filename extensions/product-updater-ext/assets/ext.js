624b4f4a7b55ecbe1686048224656daafed95804
hn-lemuel <Lemuel.Lloren@au.harveynorman.com>
2023-04-26 11:54:37 +0800

    Add script logic on extention

--- extensions/product-updater-ext/assets/ext.js
+++ extensions/product-updater-ext/assets/ext.js
@@ -1,1 +1,32 @@
 console.log("JS working!");
+
+const fetchData = async () => {
+  await fetch("https://8d01-49-149-67-13.ngrok-free.app/apps/product-updater", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({}),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const productIdFromInput = document.getElementById("productID").value;
+      const productIDValue = `gid://shopify/Product/${productIdFromInput}`;
+      console.log(productIDValue);
+
+      for (let i = 0; i < data.data.length; i++) {
+        if (data.data[i].product_id === productIDValue) {
+          const isHotEl = document.getElementById("appBadge");
+          isHotEl.classList.add("is-hot--hide");
+          isHotEl.classList.add("is-hot");
+          break; // exit the loop once a match is found
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+fetchData();
--- web/index.js
+++ web/index.js
@@ -43,12 +43,6 @@ app.post(
 // If you are adding routes outside of the /api path, remember to
 // also add a proxy rule for them in web/frontend/vite.config.js
 
-app.use("/express-proxy", (req, res) => {
-  res.redirect(
-    "https://rent-myspace-challenged-jason.trycloudflare.com/api/products-highlight"
-  );
-});
-
 app.use("/api/*", shopify.validateAuthenticatedSession());
 
 app.use(express.json());
